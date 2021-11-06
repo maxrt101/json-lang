f866aa23793cac2e36bc6dca83969654c83867ba
Maksym Tkachuk <s2m4f6@gmail.com>
2021-11-06 20:28:19 +0200

    Updated version in cli

--- core/cli.py
+++ core/cli.py
@@ -25,7 +25,7 @@ class Colors:
 class Repl:
     def __init__(self):
         self.env = {
-            'version': '0.1.2',
+            'version': '0.1.3',
             'prompt': '#',
             'colors': True,
             'debug': False
